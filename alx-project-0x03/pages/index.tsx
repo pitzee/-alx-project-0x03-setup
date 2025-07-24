0b556a90b7ac8507199f4677038b14b45fa1f725
pitzee <pitzee421@gmail.com>
2025-07-24 21:18:59 +0300

    Implementing a share layout

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <Header />
+      <h1>hi</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default index;
